057f2aa4798aa0f0e2802d7da5d6390146c809b1
Caleb <caleb.hunt789@gmail.com>
2026-07-28 14:54:59 +0000

    testing the code this far

--- Pizza.js
+++ Pizza.js
@@ -12,3 +12,26 @@ class Pizza {
     this.quantity = quantity;
     this.instructions = instructions || 'None';
   }
+
+  getDescription() {
+    // Turn the arrays of cheeses and toppings into readable lists
+    const cheeseList = this.cheeses.join(', ');
+    const toppingList = this.toppings.join(', ');
+
+    // Return a multi-line string with all the order details
+    return `Thank you, ${this.customerName}!
+
+    Your Order Details:
+• Quantity: ${this.quantity}
+• Size: ${this.size}
+• Crust: ${this.crust}
+• Sauce: ${this.sauce}
+• Cheese: ${cheeseList}
+• Toppings: ${toppingList}
+• Special Instructions: ${this.instructions}
+
+Contact: ${this.phone} | ${this.email}
+
+Your delicious pizza is being prepared! Enjoy! 🍕`;
+  }
+}
